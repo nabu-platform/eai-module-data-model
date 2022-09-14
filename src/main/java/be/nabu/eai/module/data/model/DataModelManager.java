8be38ac6fcb5a0f8a2f89118c8c044dc55ca1c6a
Alexander Verbruggen <alex@nabu.be>
2022-09-14 13:26:22 +0200

    added reference calculation

--- src/main/java/be/nabu/eai/module/data/model/DataModelManager.java
+++ src/main/java/be/nabu/eai/module/data/model/DataModelManager.java
@@ -1,5 +1,9 @@
 package be.nabu.eai.module.data.model;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import be.nabu.eai.repository.api.Repository;
 import be.nabu.eai.repository.managers.base.JAXBArtifactManager;
 import be.nabu.libs.resources.api.ResourceContainer;
@@ -15,4 +19,26 @@ public class DataModelManager extends JAXBArtifactManager<DataModelConfiguration
 		return new DataModelArtifact(id, container, repository);
 	}
 
+	@Override
+	public List<String> getReferences(DataModelArtifact artifact) throws IOException {
+		List<String> references = new ArrayList<String>();
+		List<String> parentReferences = super.getReferences(artifact);
+		if (parentReferences != null) {
+			references.addAll(parentReferences);
+		}
+		List<DataModelEntry> entries = artifact.getConfig().getEntries();
+		if (entries != null) {
+			for (DataModelEntry entry : entries) {
+				if (entry.getType() != null) {
+					String id = entry.getType().getId();
+					if (id != null && !references.contains(id)) {
+						references.add(id);
+					}
+				}
+			}
+		}
+		return references;
+	}
+
+	
 }
